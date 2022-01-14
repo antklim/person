b176686e016d0dda08ea00bb05c41294185f70f9
antklim <antklim@gmail.com>
2022-01-14 17:28:01 +1100

    Refactoring

--- name/name.go
+++ name/name.go
@@ -8,16 +8,14 @@ import "strings"
 // that have value of empty string is omitted from joining. Name parts joined
 // with a single white space separator.
 func FullName(parts []string) string {
-	p := formatAndFilter(parts, strings.TrimSpace, defaultFilter)
-	fullName := strings.Join(p, " ")
-	return fullName
+	return fullNameFormat(parts, strings.TrimSpace)
 }
 
 // FullNameDefault returns a person's full name as a result of joining parts of
 // the name. If the joining of name parts produces an empty string then default
 // value d returned.
 func FullNameDefault(parts []string, d string) string {
-	if n := FullName(parts); n != "" {
+	if n := fullNameFormat(parts, strings.TrimSpace); n != "" {
 		return n
 	}
 	return d
@@ -27,9 +25,7 @@ func FullNameDefault(parts []string, d string) string {
 // of the name. Every name part is formatted with format function f. Formatted
 // name parts then joined.
 func FullNameFormatFunc(parts []string, f func(string) string) string {
-	p := formatAndFilter(parts, f, defaultFilter)
-	fullName := strings.Join(p, " ")
-	return fullName
+	return fullNameFormat(parts, f)
 }
 
 // FullNameDefaultFormatFunc returns a person's full name as a result of joining
@@ -37,12 +33,10 @@ func FullNameFormatFunc(parts []string, f func(string) string) string {
 // Formatted name parts then joined. If the joining of name parts produces an
 // empty string then default value d returned.
 func FullNameDefaultFormatFunc(parts []string, d string, f func(string) string) string {
-	p := formatAndFilter(parts, f, defaultFilter)
-	fullName := strings.Join(p, " ")
-	if fullName == "" {
-		return d
+	if n := fullNameFormat(parts, f); n != "" {
+		return n
 	}
-	return fullName
+	return d
 }
 
 type formatFunc func(string) string
@@ -60,4 +54,10 @@ func formatAndFilter(a []string, f formatFunc, ff filterFunc) []string {
 
 func defaultFilter(s string) bool {
 	return s != ""
+}
+
+func fullNameFormat(parts []string, f func(string) string) string {
+	p := formatAndFilter(parts, f, defaultFilter)
+	fullName := strings.Join(p, " ")
+	return fullName
 }
