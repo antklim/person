b1b0899fcd101a431cfe5ec3ff643dd58184f556
antklim <antklim@gmail.com>
2022-01-29 17:09:14 +1100

    AgeOn tests inited

--- age.go
+++ age.go
@@ -1,6 +1,14 @@
 package person
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+// TODO: define format
+// TODO: AgeOn and Age should return error on invalid format
+
+var errDobIsAfterDate = errors.New("age on: date of birth is after provided date")
 
 // Age returns persons age formatted using format.
 // For example 31 years, 2 months, 1 week, and 2 days.
@@ -8,8 +16,14 @@ func Age(dob time.Time, format string) string {
 	return ""
 }
 
-func AgeIn(dob, in time.Time, format string) (string, error) {
-	return "", nil
+// AgeOn returns persons age on a specific date formatted using format.
+// It returns an error when provided date is before the date of birth (dob).
+// For example 31 years, 2 months, 1 week, and 2 days.
+func AgeOn(dob, date time.Time, format string) (string, error) {
+	if dob.After(date) {
+		return "", errDobIsAfterDate
+	}
+	return "2 days", nil
 }
 
 func IsAdult(dob time.Time, years time.Duration) bool {
---
+++ age_test.go
@@ -0,0 +1,70 @@
+package person_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/antklim/person"
+)
+
+var testDOB = time.Date(1991, time.April, 1, 13, 17, 0, 0, time.UTC)
+
+func TestAge(t *testing.T) {
+	t.Skip("not implemented")
+	// testCases := []struct {
+	// 	format   string
+	// 	expected string
+	// }{
+	// 	{
+	// 		format:   "",
+	// 		expected: "",
+	// 	},
+	// }
+}
+
+func TestAgeOn(t *testing.T) {
+	testCases := []struct {
+		date     time.Time
+		format   string
+		expected string
+	}{
+		{
+			date:     time.Date(1991, time.April, 3, 13, 17, 0, 0, time.UTC),
+			format:   "D",
+			expected: "2 days",
+		},
+	}
+
+	for _, tC := range testCases {
+		got, err := person.AgeOn(testDOB, tC.date, tC.format)
+		if err != nil {
+			t.Errorf("AgeOn(%s, %s, %s) failed: %v", testDOB.Format(time.RFC3339),
+				tC.date.Format(time.RFC3339), tC.format, err)
+		} else if got != tC.expected {
+			t.Errorf("AgeOn(%s, %s, %s) = %s, want %s", testDOB.Format(time.RFC3339),
+				tC.date.Format(time.RFC3339), tC.format, got, tC.expected)
+		}
+	}
+}
+
+func TestAgeOnFails(t *testing.T) {
+	format := "D"
+	date := testDOB.Add(-time.Second)
+	expected := errors.New("age on: date of birth is after provided date")
+
+	got, err := person.AgeOn(testDOB, date, format)
+	if err == nil {
+		t.Fatalf("AgeOn(%s, %s, %s) = %s, want to fail with error: %s", testDOB.Format(time.RFC3339),
+			date.Format(time.RFC3339), format, got, expected.Error())
+	} else {
+		if err.Error() != expected.Error() {
+			t.Errorf("AgeOn(%s, %s, %s) failed %v, want to fail due to %v",
+				testDOB.Format(time.RFC3339), date.Format(time.RFC3339), format, err, expected)
+		}
+		if got != "" {
+			t.Errorf("AgeOn(%s, %s, %s) = %s, want formatted date to be an empty string",
+				testDOB.Format(time.RFC3339), date.Format(time.RFC3339), format, got)
+		}
+	}
+}
